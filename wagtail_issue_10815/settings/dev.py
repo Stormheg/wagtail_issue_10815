c83a0a7325f0a1168dcccdf23f58e081aab1d8ad
Storm B. Heg <storm@stormbase.digital>
2023-09-28 11:54:21 +0200

    init

---
+++ wagtail_issue_10815/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-_atp-nbrm&t7yv6j@arrytz$ukd@c0-rwq6fzye)!tcwi!qnch"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
